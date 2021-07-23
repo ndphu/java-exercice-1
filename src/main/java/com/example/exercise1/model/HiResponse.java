a85b6bea4e84dc8a838a5c7e34fe25665bf74843
Oanh Hoang Yen Nguyen <oanhhynguyen@kms.com.vn>
2021-07-23 20:55:48 +0700

    add files

---
+++ src/main/java/com/example/exercise1/model/HiResponse.java
@@ -0,0 +1,16 @@
+package com.example.exercise1.model;
+
+import java.time.Instant;
+
+public class HiResponse {
+    public String hi;
+
+    public String getHi() {
+        return hi;
+    }
+
+    public void setHi(String hi) {
+        this.hi = hi;
+    }
+
+}
