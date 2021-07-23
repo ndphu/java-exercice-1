cf71ca4f7cefbbbaa6394ea68559a70eb97317c3
Oanh Hoang Yen Nguyen <oanhhynguyen@kms.com.vn>
2021-07-23 18:35:32 +0700

    created sayHi endpoint

--- src/main/java/com/example/exercise1/controller/HelloWorldController.java
+++ src/main/java/com/example/exercise1/controller/HelloWorldController.java
@@ -1,11 +1,17 @@
 package com.example.exercise1.controller;
 
 import com.example.exercise1.model.MyDateTime;
+import com.example.exercise1.model.User;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloWorldController {
@@ -34,6 +40,16 @@ public class HelloWorldController {
         Date endDate = new Date(2021, 9, 05);
         long timeMilli = endDate.getTime() - startDate.getTime();
         return timeMilli;
+    }
+
+    @PostMapping(
+            value = "/api/sayHi"
+//            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public Map<String, Object> sayHi(@RequestBody User body) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("Hi", body.name);
+        return map;
     }
 
 }
